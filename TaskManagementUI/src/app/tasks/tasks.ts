29fc8d9d6a009eac757c7385cc21d47d68b636d7
Eleen <eleen.abushawish@icloud.com>
2026-08-19 23:21:33 +0300

    Add statics to UI

--- TaskManagementUI/src/app/tasks/tasks.ts
+++ TaskManagementUI/src/app/tasks/tasks.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { DatePipe, NgClass, NgFor, NgIf } from '@angular/common';
 import { Task } from '../models/task';
 import { TaskService } from '../services/task.service';
@@ -12,8 +12,25 @@ import { TaskService } from '../services/task.service';
 export class Tasks implements OnInit {
   isDarkMode = false;
   tasks: Task[] = [];
+get totalTasks(): number {
+  return this.tasks.length;
+}
 
-  constructor(private readonly taskService: TaskService) {}
+get lowPriorityTasks(): number {
+  return this.tasks.filter(task => task.priority === 1).length;
+}
+
+get mediumPriorityTasks(): number {
+  return this.tasks.filter(task => task.priority === 2).length;
+}
+
+get highPriorityTasks(): number {
+  return this.tasks.filter(task => task.priority === 3).length;
+}
+  constructor(
+    private readonly taskService: TaskService,
+    private readonly changeDetector: ChangeDetectorRef,
+  ) {}
 
   ngOnInit(): void {
     this.loadTasks();
@@ -23,6 +40,7 @@ export class Tasks implements OnInit {
     this.taskService.getTasks().subscribe({
       next: (data) => {
         this.tasks = data;
+        this.changeDetector.detectChanges();
         console.log(this.tasks);
       },
       error: (error) => {
@@ -31,7 +49,19 @@ export class Tasks implements OnInit {
     });
   }
 
+  isOverdue(dueDate: string): boolean {
+    const due = new Date(dueDate);
+    due.setHours(0, 0, 0, 0);
+
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    return due < today;
+  }
+
   toggleTheme(): void {
     this.isDarkMode = !this.isDarkMode;
   }
+
 }
+
