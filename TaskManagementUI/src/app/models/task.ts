8dc766059722889d9f5228c4e94372c95b348eda
Eleen <eleen.abushawish@icloud.com>
2026-08-19 21:10:31 +0300

    Api connected to angular

--- TaskManagementUI/src/app/models/task.ts
+++ TaskManagementUI/src/app/models/task.ts
@@ -1,5 +1,7 @@
 export interface Task {
   title: string;
   dueDate: Date;
-fullName: string | null;
+  fullName: string | null;
+  status: number;
+  priority: number;
 }
--- TaskManagementUI/src/app/tasks/tasks.ts
+++ TaskManagementUI/src/app/tasks/tasks.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
-import { NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { DatePipe, NgClass, NgFor, NgIf } from '@angular/common';
+import { Task } from '../models/task';
+import { TaskService } from '../services/task.service';
 
 @Component({
   selector: 'app-tasks',
-  imports: [NgClass],
+  imports: [DatePipe, NgClass, NgFor, NgIf],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css',
 })
-export class Tasks {
+export class Tasks implements OnInit {
   isDarkMode = false;
+  tasks: Task[] = [];
+
+  constructor(private readonly taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.loadTasks();
+  }
+
+  loadTasks(): void {
+    this.taskService.getTasks().subscribe({
+      next: (data) => {
+        this.tasks = data;
+        console.log(this.tasks);
+      },
+      error: (error) => {
+        console.error('Error loading tasks:', error);
+      },
+    });
+  }
 
   toggleTheme(): void {
     this.isDarkMode = !this.isDarkMode;
