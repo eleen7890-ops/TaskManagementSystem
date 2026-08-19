6ca570bd3cd588c0927a92ac88c976149f088776
Eleen <eleen.abushawish@icloud.com>
2026-08-19 20:33:46 +0300

    Add CORS policy for angular

---
+++ TaskManagementUI/src/app/models/task.ts
@@ -0,0 +1,7 @@
+export interface Task {
+  title: string;
+  dueDate: Date;
+fullName: string | null;
+status: number;
+priority: number;
+}
--- TaskManagementUI/src/app/tasks/tasks.ts
+++ TaskManagementUI/src/app/tasks/tasks.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgClass, NgFor, NgIf, DatePipe } from '@angular/common';
+import { TaskService } from '../services/task.service';
+import { Task } from '../models/task';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [NgClass, NgFor, NgIf, DatePipe],
   templateUrl: './tasks.html',
-  styleUrl: './tasks.css',
+  styleUrl: './tasks.css'
 })
-export class Tasks {}
+export class Tasks implements OnInit {
+
+  isDarkMode = false;
+
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.loadTasks();
+  }
+
+  loadTasks(): void {
+    this.taskService.getTasks().subscribe({
+      next: (data) => {
+        this.tasks = data;
+        console.log(this.tasks);
+      },
+      error: (error) => {
+        console.error('Error loading tasks:', error);
+      }
+    });
+  }
+
+  toggleTheme(): void {
+    this.isDarkMode = !this.isDarkMode;
+  }
+}
